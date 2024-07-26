9a64c7ef7d086205475789d96f8da8cea7af4bfd
Anjali Vibhute <101789772+VibhuteAnjali@users.noreply.github.com>
2024-07-26 19:51:40 +0530

    Update Gallery.jsx

--- src/component/Gallery.jsx
+++ src/component/Gallery.jsx
@@ -2,7 +2,15 @@
 
 import "../styles/Gallery.css";
 import { useNavigate } from "react-router-dom";
-export default function Gallery({ image, para, title, url, index, code }) {
+export default function Gallery({
+  image,
+  para,
+  title,
+  url,
+  index,
+  code,
+  websiteLink,
+}) {
   const navigate = useNavigate();
   function handleUrl() {
     navigate(`${url}`);
@@ -20,9 +28,27 @@ export default function Gallery({ image, para, title, url, index, code }) {
               {title}
             </div>
             <p>{para}</p>
-            <a href={code} className="codeBtn" target="_blank">
-              Code
-            </a>
+            <div className="buttonFlex">
+              <a href={code} className="codeBtn" target="_blank">
+                <img
+                  src="https://img.icons8.com/?size=30&id=12599&format=png&color=FFFFFF"
+                  alt="code"
+                />
+                Code
+              </a>
+              <a
+                href={websiteLink}
+                className={websiteLink ? "codeBtn" : "disabledBtn codeBtn"}
+                target="_blank"
+              >
+                <img
+                  src="https://img.icons8.com/?size=20&id=IvlXyn1yGmDS&format=png&color=FFFFFF"
+                  alt="code"
+                  className="Nocurve"
+                />
+                Link
+              </a>
+            </div>
           </div>
         </div>
       </div>
